c00c03b9bbd8b1dbb3d149b051c3ee0bc014ef9c
Emil Fresk <emil.fresk@kiteshield.com>
2025-01-14 15:33:34 +0100

    Make setters const fn

--- src/generate/fieldset.rs
+++ src/generate/fieldset.rs
@@ -97,7 +97,7 @@ pub fn render(opts: &super::Options, ir: &IR, fs: &FieldSet, path: &str) -> Resu
                         }
                         #doc
                         #[inline(always)]
-                        pub fn #name_set(&mut self, n: usize, val: #field_ty) {
+                        pub const fn #name_set(&mut self, n: usize, val: #field_ty) {
                             assert!(n < #len);
                             let offs = #off_in_reg + #offs_expr;
                             self.0 = (self.0 & !(#mask << offs)) | (((#to_bits) & #mask) << offs);
@@ -114,7 +114,7 @@ pub fn render(opts: &super::Options, ir: &IR, fs: &FieldSet, path: &str) -> Resu
                         }
                         #doc
                         #[inline(always)]
-                        pub fn #name_set(&mut self, val: #field_ty) {
+                        pub const fn #name_set(&mut self, val: #field_ty) {
                             self.0 = (self.0 & !(#mask << #off_in_reg)) | (((#to_bits) & #mask) << #off_in_reg);
                         }
                     ));
@@ -157,7 +157,7 @@ pub fn render(opts: &super::Options, ir: &IR, fs: &FieldSet, path: &str) -> Resu
                         }
                         #doc
                         #[inline(always)]
-                        pub fn #name_set(&mut self, n: usize, val: #field_ty) {
+                        pub const fn #name_set(&mut self, n: usize, val: #field_ty) {
                             assert!(n < #len);
                             #( let offs = #off_in_reg + #offs_expr;
                                self.0 = (self.0 & !(#mask << offs)) | (((#to_bits >> #off_in_val) & #mask) << offs); )*;
@@ -175,7 +175,7 @@ pub fn render(opts: &super::Options, ir: &IR, fs: &FieldSet, path: &str) -> Resu
                         }
                         #doc
                         #[inline(always)]
-                        pub fn #name_set(&mut self, val: #field_ty) {
+                        pub const fn #name_set(&mut self, val: #field_ty) {
                            #( self.0 = (self.0 & !(#mask << #off_in_reg)) | (((#to_bits >> #off_in_val) & #mask) << #off_in_reg); )*;
                         }
                     ))
